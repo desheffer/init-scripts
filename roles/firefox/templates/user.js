634153e1a28ff93e0a7960c20edf6f2e921932bc
Doug Sheffer <desheffer@gmail.com>
2020-02-01 16:21:48 -0500

    Disable smooth scroll

--- roles/firefox/templates/user.js
+++ roles/firefox/templates/user.js
@@ -1,4 +1,5 @@
 user_pref("browser.zoom.siteSpecific", false);
+user_pref("general.smoothScroll", false);
 user_pref("mousewheel.acceleration.factor", 10);
 user_pref("mousewheel.acceleration.start", 5);
 user_pref("mousewheel.min_line_scroll_amount", 160);
