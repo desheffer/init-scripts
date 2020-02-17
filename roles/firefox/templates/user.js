d9cfbc48e32b838acbba044affc00cf015bfd009
Doug Sheffer <desheffer@gmail.com>
2020-02-17 15:25:38 -0500

    Disable desktop notifications

--- roles/firefox/templates/user.js
+++ roles/firefox/templates/user.js
@@ -3,3 +3,4 @@ user_pref("general.smoothScroll", false);
 user_pref("mousewheel.acceleration.factor", 10);
 user_pref("mousewheel.acceleration.start", 5);
 user_pref("mousewheel.min_line_scroll_amount", 160);
+user_pref("permissions.default.desktop-notification", 2);
