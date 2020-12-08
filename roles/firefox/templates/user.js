8583f9b660ee8dd48daeb9dc704fc345105103ba
Doug Sheffer <desheffer@gmail.com>
2020-12-07 23:29:49 -0500

    Tweak Firefox config

--- roles/firefox/templates/user.js
+++ roles/firefox/templates/user.js
@@ -1,5 +1,7 @@
 user_pref("browser.zoom.siteSpecific", false);
 user_pref("general.smoothScroll", false);
+user_pref("media.hardwaremediakeys.enabled", false);
+user_pref("media.videocontrols.picture-in-picture.enabled", false);
 user_pref("mousewheel.acceleration.factor", 10);
 user_pref("mousewheel.acceleration.start", 5);
 user_pref("mousewheel.min_line_scroll_amount", 160);
