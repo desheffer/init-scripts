1410b4c1c6a825822aea7455afc959f977f170ee
Doug Sheffer <desheffer@gmail.com>
2021-12-19 13:52:45 -0500

    Disable Firefox Pocket

--- roles/firefox/templates/user.js
+++ roles/firefox/templates/user.js
@@ -1,4 +1,5 @@
 user_pref("browser.zoom.siteSpecific", false);
+user_pref("extensions.pocket.enabled", false);
 user_pref("general.smoothScroll", false);
 user_pref("media.videocontrols.picture-in-picture.enabled", false);
 user_pref("permissions.default.desktop-notification", 2);
