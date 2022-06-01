03017a089fc7e6e199b96fe945675887ea11c0a7
Doug Sheffer <desheffer@gmail.com>
2022-06-01 01:27:33 -0400

    Disable hardwaremediakeys in Firefox

--- roles/firefox/templates/user.js
+++ roles/firefox/templates/user.js
@@ -1,5 +1,6 @@
 user_pref("browser.zoom.siteSpecific", false);
 user_pref("extensions.pocket.enabled", false);
 user_pref("general.smoothScroll", false);
+user_pref("media.hardwaremediakeys.enabled", false);
 user_pref("media.videocontrols.picture-in-picture.enabled", false);
 user_pref("permissions.default.desktop-notification", 2);
