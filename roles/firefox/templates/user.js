7f01484d93a7606fa5a779a94834bb64cef072af
Doug Sheffer <desheffer@gmail.com>
2022-07-26 01:46:30 -0400

    Remove Firefox

--- roles/firefox/templates/user.js
+++
@@ -1,6 +0,0 @@
-user_pref("browser.zoom.siteSpecific", false);
-user_pref("extensions.pocket.enabled", false);
-user_pref("general.smoothScroll", false);
-user_pref("media.hardwaremediakeys.enabled", false);
-user_pref("media.videocontrols.picture-in-picture.enabled", false);
-user_pref("permissions.default.desktop-notification", 2);
